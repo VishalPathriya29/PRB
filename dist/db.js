d29ba51c3b027f7689860f4b2c9514dc0ae4ae12
vishal pathriya <vishalpathria29@gmail.com>
2024-08-22 00:26:54 +0530

    dist

--- dist/db.js
+++ dist/db.js
@@ -8,7 +8,7 @@ require("dotenv/config");
 const pool = mysql2_1.default.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    // port: 23621,
+    port: 23621,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     // port: process.env.DB_PORT,
